5e6425c22d72daf2b83bc0328fc270a82a6d45c7
ahhyeon <ahhyeon27@gmail.com>
2022-05-09 16:54:34 +0900

    버튼 만들긴 했는데 아래로 안 내려감

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     int value =0;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
